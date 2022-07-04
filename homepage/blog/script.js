9ae9a0c27ab4c311569575e445fbdccb74310354
lime360 entertainment <56405352+lime360@users.noreply.github.com>
2022-07-04 19:12:27 +0300

    new damn blog post

--- homepage/blog/script.js
+++ homepage/blog/script.js
@@ -31,6 +31,7 @@ let authorLink = "https://lime360.neocities.org/"; // Enter your website, social
   use a date it must still follow that format.*/
 
 let postsArray = [
+["posts/2022-07-04-The-future-of-the-website.html"],
 ["posts/2022-06-26-Nanoblog.html"],
 ["posts/2022-06-06-Site-is-now-automated!.html"],
 [ "posts/2022-04-09-My-second-website-is-finally-here!.html" ],
